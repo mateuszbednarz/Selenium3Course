f194582fbaaedb6bdd8bda159dc992905f74fb9f
Mateusz Bednarz <mateusz.bednarz@gmail.com>
2019-11-05 16:37:55 +0100

    Add files - 05.11.2019 (XPath's)

--- src/test/java/FirstSeleniumTest.java
+++ src/test/java/FirstSeleniumTest.java
@@ -3,24 +3,30 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
+import static org.openqa.selenium.By.cssSelector;
+
 public class FirstSeleniumTest {
 
     @Test
     public void googleOpenTest()
     {
-        String driverPath = "E:\\mav-temp-iji\\Selenium3Course\\src\\main\\resources\\executables\\drivers\\chromedriver.exe";
+        // SOFLAB CONFIG
+        String driverPath = "C:\\Users\\mateusz.bednarz\\Documents\\IntelliJ-Projects\\src\\main\\resources\\executables\\drivers\\chromedriver.exe";
+
         System.setProperty("webdriver.chrome.driver", driverPath);
         //Dimension dimension = new Dimension(1300, 700); // web-browser window dimension parameters
         ChromeDriver driver = new ChromeDriver();
         //driver.manage().window().setSize(dimension);
         driver.manage().window().maximize();
-        driver.get("E://mav-temp-iji//pliki-dolaczone-do-kursu//Test.html");
+
+        // SOFLAB CONFIG
+        driver.get("C://Users//mateusz.bednarz//Documents//SelCourseTestWebPage//Test.html");
+
         //driver.findElement(By.id("newPage")).click();
         //driver.close(); // zamyka okno na którym jest focus (obecnie znajduje się driver)
         //driver.quit(); // zamyka wszystkie otwarte okna przeglądarki
 
         //WebElement firstnameInput = driver.findElement(By.name("fname"));
-
         //WebElement clickOnMeButton = driver.findElement(By.id("clickOnMe"));
         //WebElement clickOnMeButton2 = driver.findElementById("clickOnMe");
 
@@ -28,5 +34,10 @@ public class FirstSeleniumTest {
         WebElement googleLink = driver.findElement(By.partialLinkText("IamWeirdLink"));
         WebElement topSecretParagraph = driver.findElement(By.className("topSecret"));
         WebElement firstInput = driver.findElement(By.tagName("input"));
+
+        WebElement clickOnMeCssButton = driver.findElement(By.cssSelector("#clickOnMe"));
+        WebElement links = driver.findElement(By.cssSelector("a"));
+        WebElement topSecretCss = driver.findElement(By.cssSelector(".topSecret"));
+        WebElement tdFirstChild = driver.findElement(By.cssSelector("td:first-child"));
     }
 }
